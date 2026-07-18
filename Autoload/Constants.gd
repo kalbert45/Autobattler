a074622e20879ab70bd64e98acb3e76c7ed59f50
Albert Kim <albertkimgames@gmail.com>
2026-07-18 16:07:26 -0400

    implemented tooltips

--- Autoload/Constants.gd
+++ Autoload/Constants.gd
@@ -1,8 +1,8 @@
 extends Node
 
-const NAME_TO_RESOURCE_PATH = {
-	"UNIT_WOLF" : "uid://df8dsrqf4htry",
-	"UNIT_ARCHER" : "",
-	"UNIT_BEAR" : "",
-	
+const COLORS = {
+	HEALTH_RED = Color("ea4f36"),
+	SHIELD_GREEN = Color("cddf6c"),
+	AP_BLUE = Color("8fd3ff"),
+	MANA_BLUE = Color("4d9be6"),
 }
--- Common/UI/Tooltips/tooltip.gd
+++ Common/UI/Tooltips/tooltip.gd
@@ -1,7 +1,10 @@
 class_name Tooltip extends PanelContainer
 
 func set_tooltip(obj) -> void:
-	pass
+	obj.accept_tooltip(self)
 	
 func set_unit_tooltip(unit : Unit) -> void:
-	pass
+	%Tier.text = str(unit.data.tier)
+	%NameLabel.text = unit.data.name
+	%Price.text = str(unit.data.price)
+	%TTLabel.text = unit.data.tooltip
